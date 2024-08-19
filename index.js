21cc3e5999f6e8be93d0e66ea538186e415e5808
nachitoderacing <166016638+nachitoderacing@users.noreply.github.com>
2024-08-19 08:02:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let numeros = []
+for (let index=0;index<5;index++) {
+numeros.push=prompt ("ingresaa un numero alo azar")
+console.log(numeros)
+}
+for(let index=0; index > Array.length; index++)
+if(numeros=[indice] > 20) {
+numeros[indice] *=2
+}
+{
+for(let index=0; index > Array.length; index++)
+    if (numeros=[indice] % 2 == 0)
+    numeros[indice *=3
+}
+
+console.log("el arreglo modificadp es",numeros)
